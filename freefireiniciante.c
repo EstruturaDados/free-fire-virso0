4040386169ede50229763d1188a505c8fbcae5dd
will <wilsonluizzzz35@gmail.com>
2026-05-22 03:50:33 +0000

    função para listar os itens

--- freefireiniciante.c
+++ freefireiniciante.c
@@ -10,3 +10,21 @@ struct Item {
     char tipo[20];
     int quantidade;
 };
+
+// Função para listar todos os itens cadastrados
+void listarItens(struct Item mochila[], int totalItens) {
+    printf("\n========== MOCHILA ==========\n");
+
+    if (totalItens == 0) {
+        printf("A mochila esta vazia.\n");
+    } else {
+        for (int i = 0; i < totalItens; i++) {
+            printf("\nItem %d\n", i + 1);
+            printf("Nome: %s\n", mochila[i].nome);
+            printf("Tipo: %s\n", mochila[i].tipo);
+            printf("Quantidade: %d\n", mochila[i].quantidade);
+        }
+    }
+
+    printf("=============================\n");
+}
