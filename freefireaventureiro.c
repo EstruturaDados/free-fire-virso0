8cbb912bf753a0fcb7e944e0179284d5e5b0daf0
will <wilsonluizzzz35@gmail.com>
2026-05-28 04:52:43 +0000

    segunda parte

--- freefireaventureiro.c
+++ freefireaventureiro.c
@@ -43,3 +43,76 @@ void listarItens(struct Item mochila[], int totalItens) {
         printf("Quantidade: %d\n", mochila[i].quantidade);
     }
 }
+// Busca sequencial
+int buscarItem(struct Item mochila[], int totalItens, char nome[]) {
+
+    for(int i=0;i<totalItens;i++) {
+
+        if(strcmp(mochila[i].nome,nome)==0)
+            return i;
+    }
+
+    return -1;
+}
+
+// Cadastro
+void cadastrarItem(struct Item mochila[], int *totalItens) {
+
+    if(*totalItens >= MAX_ITENS) {
+
+        printf("\nLimite atingido!\n");
+        return;
+    }
+
+    printf("\nNome: ");
+    scanf(" %[^\n]", mochila[*totalItens].nome);
+
+    printf("Tipo: ");
+    scanf(" %[^\n]", mochila[*totalItens].tipo);
+
+    printf("Quantidade: ");
+    scanf("%d",&mochila[*totalItens].quantidade);
+
+    (*totalItens)++;
+
+    listarItens(mochila,*totalItens);
+}
+
+// Remover vetor
+void removerItem(struct Item mochila[], int *totalItens) {
+
+    char nome[30];
+
+    printf("\nItem para remover: ");
+    scanf(" %[^\n]",nome);
+
+    int pos=
+    buscarItem(
+    mochila,
+    *totalItens,
+    nome
+    );
+
+    if(pos==-1) {
+
+        printf("\nNao encontrado\n");
+        return;
+    }
+
+    for(
+    int i=pos;
+    i<*totalItens-1;
+    i++
+    ) {
+
+        mochila[i]=
+        mochila[i+1];
+    }
+
+    (*totalItens)--;
+
+    listarItens(
+    mochila,
+    *totalItens
+    );
+}
